f10a20b1834ac6efff0d8ecaae7c6972e09a81d1
dezhizhang <1541609448@qq.com>
2022-08-02 04:46:53 +0800

    feat:update

--- src/vdom/patch.js
+++ src/vdom/patch.js
@@ -5,23 +5,38 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-07-26 06:43:13
  * :last editor: 张德志
- * :date last edited: 2022-07-31 08:34:19
+ * :date last edited: 2022-08-02 04:46:22
  */
 
 
 export function patch(oldVnode,vnode) {
-    // 将虚拟节点转换成真实节点
-    let el = createElm(vnode);
-    updateProperties(vnode);
-    let parentElm = oldVnode.parentNode;
-    parentElm.insertBefore(el,oldVnode.nextSibling);
-    // 删除老节点
-    parentElm.removeChild(oldVnode);
-    return el;
+    
+    if(oldVnode.nodeType === 1) {
+        // 将虚拟节点转换成真实节点
+        let el = createElm(vnode);
+        updateProperties(vnode);
+        let parentElm = oldVnode.parentNode;
+        parentElm.insertBefore(el,oldVnode.nextSibling);
+        // 删除老节点
+        parentElm.removeChild(oldVnode);
+        return el;
+    }else {
+        //1.比较两个元素标签不一样直接替换
+        if(oldVnode.tag !== vnode.tag) {
+            return oldVnode.el.parentNode.removeChild(createElm(vnode))
+        }
+        //2文本的比对
+        if(!oldVnode.tag) {
+            if(oldVnode.text !== vnode.text) {
+                return oldVnode.el.textContent = vnode.text;
+            }
+        }
+    }
+
 }
 
 
-function createElm(vnode) {
+export function createElm(vnode) {
     let { tag,children,key,data,text} = vnode;
     if(typeof tag === 'string') {
         vnode.el = document.createElement(tag);
